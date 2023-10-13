920772ea272fac40d4f582813435b6cc04c0d78c
최정필 <wjdvlf1407@gmail.com>
2023-10-13 15:46:42 +0900

    11

---
+++ src/com/javaex/ex01/Customer.java
@@ -0,0 +1,44 @@
+package com.javaex.ex01;
+
+public class Customer extends Person {
+
+	// 필드
+	private int cNo;
+	private int point;
+
+	// 생성자
+	public Customer() {
+
+	}
+
+	public Customer(String name, String hp, int cNo, int point) {
+		super(name, hp);
+		this.cNo = cNo;
+		this.point = point;
+
+	}
+
+	// 메소드 - gs
+	public int getcNo() {
+		return cNo;
+	}
+
+	public int getPoint() {
+		return point;
+	}
+
+	public void setcNo(int cNo) {
+		this.cNo = cNo;
+	}
+
+	public void setPoint(int point) {
+		this.point = point;
+	}
+
+	// 메소드 - 일반
+	public void showInfo() {
+		System.out.println("#이름: " + name + ", #핸드폰: " + hp + "#고객번호: " + cNo + "#포인트점수: " + point);
+
+	}
+
+}
---
+++ src/com/javaex/ex01/PersonApp.java
@@ -0,0 +1,17 @@
+// 작성자: 02_짝수_최정필
+
+package com.javaex.ex01;
+
+public class PersonApp {
+
+	public static void main(String[] args) {
+
+		Person p = new Person("정우성", "010-1111-2222");
+		Customer s = new Customer("유재석", "010-2222-3333", 1, 1000);
+
+		p.showInfo();
+		s.showInfo();
+
+	}
+
+}
